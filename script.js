c76b6b4b4bfd909356b6e2905ba1d38ac4375d49
vivekc <chaudharivivek.512@gmail.com>
2023-08-27 23:56:47 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+const display = document.querySelector(".display");
+const buttons = document.querySelectorAll("button");
+const specialChars = ["%", "*", "/", "-", "+", "="];
+let output = "";
+
+//Define Function to calculate based on button clicked.
+const calculate = (btnValue) => {
+    if (btnValue === "=" && output !== "") {
+        output = eval(output.replace("%", "/100"));
+    } else if (btnValue === "AC") {
+        output = ""
+    } else if (v = btnValue === "DEL") {
+        output = output.toString().slice(0, -1);
+    } else {
+        if (output === "" && specialChars.includes(btnValue)) return;
+        output += btnValue;
+    }
+    display.value = output;
+}
+//Event Listener
+
+buttons.forEach(button => {
+    button.addEventListener("click", (e) => calculate(e.target.dataset.value))
+})
